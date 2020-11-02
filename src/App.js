29f65a2cb981486f1f4dafb0d9790bdca2ffb8df
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-02 21:27:14 +0500

    navbar2 updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navebar from './components/Navbar/Navbar';
 import Navbar2 from './components/Navbar2/Navbar2';
--- src/components/Navbar2/Navbar2.js
+++ src/components/Navbar2/Navbar2.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import style from './navbar2.module.css';
+import image from './images/bg_image.jpg'
+
+
 
 function Navbar2() {
     return (
         <div>
-            <h1>hello world</h1>
+            <Navbar  className={style.ribbon} bg="light" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavDropdown title="All Categories" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1"><b>Mobiles</b></NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Tablets</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Mobiles Phones</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Accessories</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Accessories</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Accessories</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#home">Mobile Phones</Nav.Link>
+                        <Nav.Link href="#link">Cars</Nav.Link>
+                        <Nav.Link href="#link">Motorcycles</Nav.Link>
+                        <Nav.Link href="#link">Houses</Nav.Link>
+                        <Nav.Link href="#link">TV - Video - Audio</Nav.Link>
+                        <Nav.Link href="#link">Tables</Nav.Link>
+                        <Nav.Link href="#link">Land & Plots</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <img className={style.bannerimg} src={image}></img>
         </div>
     )
 }
 
-export default Navbar2
+export default Navbar2;
