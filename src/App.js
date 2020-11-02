21f5b567715f82bec43b01ffd4e598c310328e92
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-03 01:04:12 +0500

    updated footer2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navebar from './components/Navbar/Navbar';
 import Navbar2 from './components/Navbar2/Navbar2';
 import Footer from './components/Footer/Footer';
+import Footer2 from './components/Footer2/Footer2';
 
 function App() {
   return (
     <div>
       <Navebar />
       <Navbar2 />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
+      <Footer2 />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { Row, Container, Col } from 'react-bootstrap'
 function Footer() {
     return (
         <div className={style.poster}>
-            <Container>
+            <Container fluid>
                 <Row>
                     <Col col={3}>
                         <img alt='img1' src="https://statics.olx.com.pk/external/base/img/phone-app.webp"></img>
@@ -19,7 +19,6 @@ function Footer() {
                         <Row>
                             <Col col={4}><img alt='img2' src="https://statics.olx.com.pk/external/base/img/appstore_2x.webp"></img></Col>
                             <Col col={4}><img alt='img3' src="https://statics.olx.com.pk/external/base/img/playstore_2x.webp"></img></Col>
-
                         </Row>
                     </Col>
                 </Row>
