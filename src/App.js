208cf51b5559b4da9255a40d263ec3486d6cc109
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-02 19:04:47 +0500

    navbar updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navebar from './components/Navbar/Navbar';
-import Poster from './components/Poster/Poster';
 
 function App() {
   return (
     <div>
       <Navebar />
-      <Poster />
       <h1>hello world</h1>
     </div>
   );
